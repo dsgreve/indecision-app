fa6e7eac0fb98e88717eb4f5dabd8891b2b83d38
Dale Greve <dale@dalegreve.com>
2018-11-18 14:34:57 -0500

    explored arrow functions and map array

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,31 +1,29 @@
-'use strict';
-
 //arguements object - no longer bound with arrow functions
 
 // es5 example of arguements
-var add = function add(a, b) {
+const add = function(a, b) {
     return a + b;
 };
-console.log(add(10, 30));
+console.log(add(10,30));
 
 //es6 example - if you require arguements use
-var addTwo = function addTwo(a, b) {
+const addTwo = (a, b) => {
     return a + b;
 };
 
-console.log(addTwo(10, 15));
+console.log(addTwo(10,15));
 
 //this keyword is - no longer bound
 // es5 example uses that = this as a work around for binding This
-var user = {
+const user = {
     name: 'Scott',
     cities: ['Troy', 'Muskegon', 'Whitehall', 'Marquette'],
-    printPlacesLived: function printPlacesLived() {
-        var that = this;
+    printPlacesLived: function () {
+        const that = this;
         console.log(this.name);
         console.log(this.cities);
-
-        this.cities.forEach(function (city) {
+        
+        this.cities.forEach(function(city) {
             console.log(that.name + ' has lived in ' + city);
         });
     }
@@ -34,42 +32,34 @@ user.printPlacesLived();
 
 // ES6 Arrow function example - arrow function does not bind its own this value 
 // You wouldn't use arrow function in a method
-var reader = {
+const reader = {
     name: 'Scott',
     books: ['American Gods', 'IT', 'Leviathan', 'The Bad Place'],
-    printBooksRead: function printBooksRead() {
-        var _this = this;
-
-        //es6 method
+    printBooksRead() { //es6 method
         //Map is an array method like forEach
         //Foreach allows you to get each item, Map allows you to transform each item
-        return this.books.map(function (book) {
-            return _this.name + ' has read ' + book;
-        });
+        return this.books.map((book) => this.name + ' has read ' + book);
         //short hand expression
 
         //for each lop example
         //this.books.forEach((book) => {
-        //console.log(this.name + ' has read ' + book);
+            //console.log(this.name + ' has read ' + book);
         //});
-    }
+    }    
 };
 console.log(reader.printBooksRead());
 
 // Challenge
-// numbers - array of numbers
-// muliplyBy - single number
-// muliply - return new arrow whare numbers have been mulitplied
-var multiplier = {
-    numbers: [2, 4, 6],
-    muliplyBy: 800,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (number) {
-            return number * _this2.muliplyBy;
-        });
-    }
+  // numbers - array of numbers
+  // muliplyBy - single number
+  // muliply - return new arrow whare numbers have been mulitplied
+const multiplier = {
+  numbers: [2, 4, 6],
+  muliplyBy: 800,
+  multiply() {
+    return this.numbers.map((number) => number * this.muliplyBy);
+  }
+  
 };
 
 console.log(multiplier.multiply());
